00e469399a88800b0682a3740c6f9b4f930158f6
6135 <guibscosta@hotmail.com>
2019-11-24 02:02:54 +0000

    stuff

--- resources/views/register_template.blade.php
+++ resources/views/register_template.blade.php
@@ -11,28 +11,7 @@
 
 <body>
   <!-- Header -->
-  <nav class="navbar navbar-inverse">
-    <div class="container-fluid">
-      <div class="navbar-header">
-        <a class="navbar-brand active" href="{$href0}"><i class="fas fa-bars" style="color: white"></i>&nbsp;{$Menu0}</a>
-      </div>
-      <ul class="nav navbar-nav navbar-right">
-        {if isset($Menu1)}<li><a href="{$href1}">{$Menu1}</a></li>{/if}
-        {if isset($Menu2)}<li><a href="{$href2}">{$Menu2}</a></li>{/if}
-        {if isset($Menu3)}<li><a href="{$href3}">{$Menu3}</a></li>{/if}
-      </ul>
-      <form class="navbar-form navbar-right" action="/action_page.php">
-        <div class="input-group">
-          <input type="text" class="form-control" placeholder="Search Topics" name="search">
-          <div class="input-group-btn">
-            <button class="btn btn-default" type="submit">
-                    <i class="fas fa-search"></i>
-                </button>
-          </div>
-        </div>
-      </form>
-    </div>
-  </nav>
+  @include('layouts.navbar')
   <!-- EOHeader -->
 
   <!-- Main Content -->
