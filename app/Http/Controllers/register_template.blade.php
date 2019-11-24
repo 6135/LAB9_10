9b11f806458ff3c12e5fdb8e72a61d599f883b2a
6135 <guibscosta@hotmail.com>
2019-11-24 02:00:17 +0000

    Create register_template.blade.php

---
+++ app/Http/Controllers/register_template.blade.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/indexStyle.css">
+  <link rel="stylesheet" href="https://streamonion.com/kinnith/fontawesome/css/all.css"></link>
+  <title>Forum</title>
+</head>
+
+<body>
+  <!-- Header -->
+  <nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <a class="navbar-brand active" href="{$href0}"><i class="fas fa-bars" style="color: white"></i>&nbsp;{$Menu0}</a>
+      </div>
+      <ul class="nav navbar-nav navbar-right">
+        {if isset($Menu1)}<li><a href="{$href1}">{$Menu1}</a></li>{/if}
+        {if isset($Menu2)}<li><a href="{$href2}">{$Menu2}</a></li>{/if}
+        {if isset($Menu3)}<li><a href="{$href3}">{$Menu3}</a></li>{/if}
+      </ul>
+      <form class="navbar-form navbar-right" action="/action_page.php">
+        <div class="input-group">
+          <input type="text" class="form-control" placeholder="Search Topics" name="search">
+          <div class="input-group-btn">
+            <button class="btn btn-default" type="submit">
+                    <i class="fas fa-search"></i>
+                </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </nav>
+  <!-- EOHeader -->
+
+  <!-- Main Content -->
+  <!-- Content -->
+  <div class="container" style="max-width: 50%">
+    <div class="well">
+      <h2 class="text-center">Register</h2>
+      <hr>
+
+      <form class="form-horizontal" action="register_action.php" method="POST">
+        <div class="form-group">
+          <label class="control-label col-sm-3" for="username">Username:</label>
+          <div class="col-sm-6">
+            <input type="text" class="form-control" id="username" placeholder="Username" name="Username" value="{$UserName}" maxlength="32" required>
+          </div>
+        </div>
+        <div class="form-group">
+          <label class="control-label col-sm-3" for="email">Email:</label>
+          <div class="col-sm-6">
+            <input type="email" class="form-control" id="email" placeholder="Email" name="Email" value="{$Email}" maxlength="100" required>
+           
+          </div>
+        </div>
+        <div class="form-group">
+          <label class="control-label col-sm-3" for="pwd">Password:</label>
+          <div class="col-sm-6">
+            <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="Pwd" maxlength="32" value="{$pwd}" required>
+          </div>
+        </div>
+        <div class="form-group">
+          <label class="control-label col-sm-3" for="pwd">Confirm Password:</label>
+          <div class="col-sm-6">
+            <input type="password" class="form-control" id="pwd" placeholder="Confirm password" name="CfmPwd" maxlength="32" value="{$cfmpwd}" required>
+          </div>
+        </div>
+        <div class="form-group">
+          <div class="col-sm-offset-3 col-sm-10">
+            <button type="submit" name="submit" class="btn btn-default"><i class="fas fa-check"></i>&nbsp;Register</button>
+            <button type="reset" name="reset" class="btn btn-default"><i class="fas fa-trash"></i>&nbsp;Clear</button>
+          </div>        
+        </div>
+      </form>
+
+    </div>
+    {if $isError eq 1 }
+    <div class="alert alert-danger">
+      <p style="text-align: center">{$Error}</p>
+    </div>
+    {/if}
+  </div>
+  <!-- EOContent -->
+
+  <!-- Footer -->
+  <footer>
+    <div class="row">
+      <div class="row-padded">
+        <small class="col-sm-6 text-left">@ 2019 DAW</small>
+        <small class="col-sm-6 text-right">Designed by a61172</small>
+      </div>
+    </div>
+  </footer>
+  <!--EOFooter -->
+
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
