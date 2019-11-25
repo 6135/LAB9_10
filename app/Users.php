554d298423d990acce6a4b2d9e7c996490d30d32
6135 <guibscosta@hotmail.com>
2019-11-25 13:40:12 +0000

    test

--- app/Users.php
+++ app/Users.php
@@ -10,7 +10,7 @@ class Users extends Model
 	protected $primaryKey = 'id';
 
   public static function getByEmail($email) {
-    //return Users::where('email',$email)->get();
+    return Users::where('email',$email)->get();
   }
   public function posts(){
     return $this->hasMany('app\Posts','user_id','id'); //Posts user_id matches id
